7369add57c999c137656c454399680f3225850ab
suyeon <smb1457@naver.com>
2019-04-22 09:25:51 +0900

    둘째주 - 구명조끼
    탐욕알고리즘

---
+++ day2/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int j = 0;
+        Arrays.sort(people);
+        for (int i = people.length-1; i > j; i--) {
+            if(people[i]+people[j] <= limit){
+                j += 1;
+                answer += 1;
+                }
+        }
+        return people.length-answer;
+    }
+}
